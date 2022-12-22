0e470840e34d9f1e79c6ca61b1e5ea1d8cd2e04d
Anisul <anisulhaque9391@gmail.com>
2022-12-22 13:17:42 -0500

    updated styling and layout of home page

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -16,6 +16,18 @@ import { isNewlyRegistered } from "../utils/authHandler";
 import { fetchListings } from "../utils/listingHandler";
 import { useNavigate } from "react-router-dom";
 import { handleStatusClose } from "../utils/statusHandler";
+import {
+  createTheme,
+  Divider,
+  FilledInput,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  Paper,
+  ThemeProvider,
+} from "@mui/material";
+
+import herobg from "../assets/images/herobg.jpg";
 
 function Home() {
   const navigate = useNavigate();
@@ -54,13 +66,52 @@ function Home() {
   useEffect(() => {
     fetchListings(setSaleListings, setRentListings, setLoading);
   }, []);
+  const heroBG = createTheme({
+    components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            backgroundImage: `url(${herobg})`,
+            height: "500px" /* You must set a specified height */,
+            backgroundPosition: "top left" /* Center the image */,
+            backgroundRepeat: "no-repeat" /* Do not repeat the image */,
+            backgroundSize:
+              "150%" /* Resize the background image to cover the entire container */,
+            // filter: "blur(8px)",
+          },
+        },
+      },
+    },
+  });
+
+  const inputStyleOverride = createTheme({
+    components: {
+      MuiFilledInput: {
+        styleOverrides: {
+          root: {
+            backgroundColor: "rgb(232, 241, 250)",
+            "&:hover": {
+              backgroundColor: "rgb(250, 232, 241)",
+              // Reset on touch devices, it doesn't add specificity
+              "@media (hover: none)": {
+                backgroundColor: "rgb(232, 241, 250)",
+              },
+            },
+            "&.Mui-focused": {
+              backgroundColor: "rgb(250, 241, 232)",
+            },
+          },
+        },
+      },
+    },
+  });
 
   if (loading || !isLoaded) {
     return <Spinner />;
   }
 
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="lg" sx={{ mb: 2 }}>
       {status && (
         <Status
           status={status}
@@ -68,41 +119,172 @@ function Home() {
           handleClose={handleStatusClose}
         />
       )}
-      <Box>
-        <Typography variant="h2" component="h2">
-          Know what your looking for?
-        </Typography>
-        <form>
-          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-            <TextField
-              sx={{ width: "400px", margin: "5px" }}
-              type="text"
-              name="address"
-              label="Address"
-              placeholder="Address"
-              required
-            />
-          </Autocomplete>
-          <IconButton type="submit">
-            <SearchIcon />
-          </IconButton>
-        </form>
-      </Box>
-      <Box>
-        <Typography variant="h4" component="h2">
-          Most Recent Sale Listings
-        </Typography>
-        <Stack
-          direction="row"
-          justifyContent="space-evenly"
-          alignItems="center"
+      <Box sx={{ position: "relative", height: "700px" }}>
+        <ThemeProvider theme={heroBG}>
+          <Paper elevation={2} sx={{ borderRadius: 5 }}>
+            <Box pt={10} pl={5}>
+              <Typography
+                variant="h3"
+                component="h2"
+                sx={{ zIndex: 10, color: "white", fontWeight: 500 }}
+              >
+                Search Now For
+              </Typography>
+              <Typography
+                variant="h3"
+                component="h2"
+                sx={{ zIndex: 10, color: "white", fontWeight: 500, mb: 5 }}
+              >
+                a Better Tomarrow
+              </Typography>
+              <form>
+                <Stack direction="row" alignItems="center">
+                  <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+                    <ThemeProvider theme={inputStyleOverride}>
+                      <FilledInput
+                        sx={{
+                          width: "40ch",
+                          borderRadius: 2,
+                          height: "40px",
+                        }}
+                        disableUnderline
+                        type="search"
+                        name="address"
+                        required
+                        endAdornment={
+                          <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={() => console.log("hello")}
+                              onMouseDown={() => console.log("hello")}
+                              edge="end"
+                            >
+                              <SearchIcon />
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                      />
+                    </ThemeProvider>
+                  </Autocomplete>
+                </Stack>
+              </form>
+            </Box>
+          </Paper>
+        </ThemeProvider>
+        <Paper
+          sx={{ position: "absolute", bottom: "6%", mx: 3, borderRadius: 3 }}
         >
-          {saleListings &&
-            saleListings.map((listing) => {
-              return <ListingCard key={listing.id} listing={listing} />;
-            })}
-        </Stack>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              width: "fit-content",
+              py: 5,
+              px: 2,
+              "& svg": {
+                m: 1.5,
+              },
+              "& hr": {
+                mx: 2.5,
+              },
+            }}
+          >
+            <Box>
+              <Typography
+                variant="h5"
+                component="h3"
+                sx={{ fontWeight: 700, height: "80px" }}
+              >
+                Info At Your Fingertips
+              </Typography>
+              <Typography variant="body1" component="p">
+                Be on top of all the listings that you create or the ones that
+                you favorite. Never miss an opportunity to find your home.
+              </Typography>
+            </Box>
+            <Divider orientation="vertical" flexItem />
+            <Box>
+              <Typography
+                variant="h5"
+                component="h3"
+                sx={{ fontWeight: 700, height: "80px" }}
+              >
+                Direct Contact
+              </Typography>
+              <Typography variant="body1" component="p">
+                Stop wasting time waiting for an agent or broker and start
+                taking the steps to take your search from dreaming about your
+                home to actually living in it. Contact the seller directly to
+                close the deal faster
+              </Typography>
+            </Box>
+            <Divider orientation="vertical" flexItem />
+            <Box>
+              <Typography
+                variant="h5"
+                component="h3"
+                sx={{ fontWeight: 700, height: "80px" }}
+              >
+                Listing Homes Simplified
+              </Typography>
+              <Typography variant="body1" component="p">
+                Zesty makes it easier than ever to list your homes. Don’t waste
+                time and get your home in front of new eyes.
+              </Typography>
+            </Box>
+          </Box>
+        </Paper>
       </Box>
+      <Box>
+        <Typography variant="h5" component="h3" sx={{ fontWeight: 700 }}>
+          Take the First Step
+        </Typography>
+        <Typography variant="body1" component="p">
+          Look at the newest homes available:
+        </Typography>
+        <Box p={4}>
+          <Typography variant="h6" component="h4" sx={{ fontWeight: 700 }}>
+            For Sale
+          </Typography>
+          <Stack
+            direction="row"
+            justifyContent="space-evenly"
+            alignItems="center"
+          >
+            {saleListings && saleListings.length > 0 ? (
+              saleListings.map((listing) => {
+                return <ListingCard key={listing.id} listing={listing} />;
+              })
+            ) : (
+              <Typography variant="body1" component="p">
+                Oh no! Looks like theres not listings
+              </Typography>
+            )}
+          </Stack>
+        </Box>
+        <Box p={4}>
+          <Typography variant="h6" component="h4" sx={{ fontWeight: 700 }}>
+            For Rent
+          </Typography>
+          <Stack
+            direction="row"
+            justifyContent="space-evenly"
+            alignItems="center"
+          >
+            {rentListings && rentListings.length > 0 ? (
+              rentListings.map((listing) => {
+                return <ListingCard key={listing.id} listing={listing} />;
+              })
+            ) : (
+              <Typography variant="body1" component="p">
+                Oh no! Looks like theres not listings
+              </Typography>
+            )}
+          </Stack>
+        </Box>
+      </Box>
+
+      {/* 
       <Box>
         <Typography variant="h4" component="h2">
           Most Recent Rent Listings
@@ -117,7 +299,7 @@ function Home() {
               return <ListingCard key={listing.id} listing={listing} />;
             })}
         </Stack>
-      </Box>
+      </Box> */}
     </Container>
   );
 }
