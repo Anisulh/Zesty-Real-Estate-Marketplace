f235a1d70cb0262df0f46382e33956a9db407bf6
Anisul <anisulhaque9391@gmail.com>
2022-12-24 08:02:06 -0500

    user can update profile information

--- src/page/Profile.tsx
+++ src/page/Profile.tsx
@@ -1,42 +1,184 @@
 import { AddRounded } from "@mui/icons-material";
-import { Avatar, Button, IconButton, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Box, Container, Stack } from "@mui/system";
-import { getAuth } from "firebase/auth";
-import { useState } from "react";
+import { getAuth, updateEmail, updateProfile } from "firebase/auth";
+import { ChangeEvent, FormEvent, useState } from "react";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+import { StatusType } from "../types";
+import Status from "../components/Status";
+import { handleStatusClose } from "../utils/statusHandler";
+import { useNavigate } from "react-router-dom";
+
+interface EditProfileType {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
 
 function Profile() {
   const auth = getAuth();
+  const navigate = useNavigate();
+  const [status, setStatus] = useState<StatusType>({
+    open: false,
+    error: false,
+    message: "",
+  });
   const [profileData, setProfileData] = useState({
     name: auth.currentUser?.displayName,
     email: auth.currentUser?.email,
   });
   const { name, email } = profileData;
+  const [editProfile, setEditProfile] = useState(false);
+  const [editProfileData, setEditProfileData] = useState<EditProfileType>({
+    firstName: name?.split(" ")[0] ?? "",
+    lastName: name?.split(" ")[1] ?? "",
+    email: email ?? "",
+  });
+  const { firstName, lastName } = editProfileData;
+
+  const onFormChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEditProfileData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(editProfileData);
+    if (auth.currentUser) {
+      if (name !== `${firstName} ${lastName}`) {
+        try {
+          await updateProfile(auth.currentUser, {
+            displayName: `${firstName} ${lastName}`,
+          });
+        } catch (error) {
+          setStatus({
+            open: true,
+            error: true,
+            message: "Unable to update profile",
+          });
+        }
+      }
+      if (email !== editProfileData.email) {
+        try {
+          await updateEmail(auth.currentUser, editProfileData.email);
+        } catch (error) {
+          setStatus({
+            open: true,
+            error: true,
+            message: "Unable to update profile",
+          });
+        }
+      }
+    }
+
+    setEditProfile(false);
+    navigate("/profile");
+  };
+
   return (
     <Container maxWidth="md">
-      <Stack direction="row" justifyContent="space-between" alignItems="start">
-        <Stack direction="row" gap={5} alignItems="center">
-          <Avatar sx={{ width: 150, height: 150 }}>
-            <IconButton sx={{ width: 150, height: 150 }}>
-              <AddRounded />
-            </IconButton>
-          </Avatar>
-          <Stack direction="column">
-            <Typography variant="h4" component="h3" sx={{ fontWeight: "bold" }}>
-              {name}
-            </Typography>
-            <Typography variant="body2" component="p">
-              {email}
-            </Typography>
-            <Typography variant="body2" component="p">
-              Properties Listed:
-            </Typography>
+      {status && (
+        <Status
+          status={status}
+          setStatus={setStatus}
+          handleClose={handleStatusClose}
+        />
+      )}
+      {editProfile ? (
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="start"
+        >
+          <form id="profileForm" onSubmit={onFormSubmit}>
+            <Stack direction="row" gap={5} alignItems="center" mt={5}>
+              <IconButton
+                aria-label="upload picture"
+                component="label"
+                sx={{ width: 150, height: 150 }}
+              >
+                <AddRounded />
+                <input hidden accept=".jpg,.png,.jpeg" type="file" />
+              </IconButton>
+
+              <Stack direction="column" gap={5}>
+                <Stack direction="row" justifyContent="space-between" gap={5}>
+                  <TextField
+                    label="First Name"
+                    name="firstName"
+                    required
+                    value={firstName}
+                    onChange={onFormChange}
+                  />
+                  <TextField
+                    label="Last Name"
+                    name="lastName"
+                    required
+                    value={lastName}
+                    onChange={onFormChange}
+                  />
+                </Stack>
+
+                <TextField
+                  label="Email"
+                  name="email"
+                  required
+                  value={editProfileData.email}
+                  onChange={onFormChange}
+                />
+              </Stack>
+            </Stack>
+          </form>
+          <Stack direction="column" justifyContent="space-between">
+            <Button type="button" onClick={() => setEditProfile(false)}>
+              Cancel
+            </Button>
+            <Button type="submit" form="profileForm">
+              Submit
+            </Button>
           </Stack>
         </Stack>
-        <IconButton>
-          <ModeEditOutlineOutlinedIcon />
-        </IconButton>
-      </Stack>
+      ) : (
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="start"
+        >
+          <Stack direction="row" gap={5} alignItems="center">
+            <Avatar sx={{ width: 150, height: 150 }}>
+              <IconButton sx={{ width: 150, height: 150 }}>
+                <AddRounded />
+              </IconButton>
+            </Avatar>
+            <Stack direction="column">
+              <Typography
+                variant="h4"
+                component="h3"
+                sx={{ fontWeight: "bold" }}
+              >
+                {name}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {email}
+              </Typography>
+              <Typography variant="body2" component="p">
+                Properties Listed:
+              </Typography>
+            </Stack>
+          </Stack>
+          <IconButton onClick={() => setEditProfile(true)}>
+            <ModeEditOutlineOutlinedIcon />
+          </IconButton>
+        </Stack>
+      )}
 
       <Box mt={5}>
         <Typography
@@ -74,7 +216,7 @@ function Profile() {
               {" "}
               See All
             </Button>
-            <IconButton sx={{ backgroundColor: "#323232" }}>
+            <IconButton onClick={() => navigate('/create-listing')} sx={{ backgroundColor: "#323232" }}>
               <AddRounded sx={{ color: "white" }} />
             </IconButton>
           </Box>
